860c6050855216afa716399d1829db244e8c3d0d
DJArgoho <57314254+djargoho@users.noreply.github.com>
2020-01-18 22:24:02 +0900

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 /*
 
-loader 방식이 안좋기 때문.
+loader 방식이 안좋기 때문..
 그럼 스타일링은 어떻게?
 스타일링에 있어선 가장 추천하는 방식은 styled-jsx 를 사용하는것입니다. 
 JS 내부에 CSS를 작성 할 수 있게 해주는 모듈인데요, 
